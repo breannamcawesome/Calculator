fcc6bc69929e7bf3d3b620339edc05375c879c55
breannamcawesome <breannamcawesome@gmail.com>
2024-11-20 14:45:25 -0800

    Number buttons nearly functional, updated screen css

---
+++ calculator.js
@@ -0,0 +1,63 @@
+//need to make buttons display on the "screen" div
+//need result to be converted to numOne for additional operations
+//need to allow only one character in the display at a time
+const screen = document.getElementById("screen");
+
+const numButton = document.getElementsByClassName("number");
+const numValue = document.getElementsByClassName("number").value;
+
+    numButton[0].addEventListener('click', () =>  {
+        
+        screen.textContent = numValue;
+
+
+    });
+
+
+const add = function(numOne, numTwo) {
+	let currentNum = numOne + numTwo;
+  return currentNum;
+};
+
+const subtract = function(numOne, numTwo) {
+  let currentNum = numOne - numTwo;
+  return currentNum;
+	
+};
+
+const sum = function(array) {
+  return array.reduce((total, current) => (total + current, 0));
+	
+};
+
+const multiply = function(array) {
+  return array.reduce((product, current) => (product * current))
+
+};
+
+
+function operate(numOne, operator, numTwo) {
+    switch (operator) {
+        case "+":
+            return add(numOne, numTwo);
+        case "-":
+            return subtract(numOne, numTwo);
+        case "*":
+            return multiply(numOne, numTwo);
+        case "-":
+            return subtract(numOne, numTwo);
+    }
+};
+
+
+let currentNum = "";
+let operator = "";
+let previousNum = "";
+
+//const numberButtons = Array.from(document.querySelectorAll(".number"));
+//numberButtons.addEventListener('click', () => {
+//    screen.textContent = 'button.value';
+//});
+
+//const operatorButtons = Array.from(document.querySelectorAll(".operator"));
+const equalButton = Array.from(document.querySelectorAll("equal"));
